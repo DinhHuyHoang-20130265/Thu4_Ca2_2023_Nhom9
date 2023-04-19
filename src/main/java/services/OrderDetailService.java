211d275c540d7426b3f3772fe8fd783a5f26b059
DinhHuyHoang-20130265 <20130265@st.hcmuaf.eduvn>
2023-04-19 10:01:51 +0700

    Thêm giao diện danh sách đơn hàng, đơn hàng đã xóa, chức năng xóa đơn hàng, xóa hard đơn hàng, khôi phục đơn hàng

---
+++ src/main/java/services/OrderDetailService.java
@@ -0,0 +1,37 @@
+package services;
+
+import DAO.OrderDAO;
+import DAO.OrderDetailDAO;
+import beans.order.OrderDetail;
+
+import java.util.List;
+
+public class OrderDetailService {
+    private static OrderDetailService orderDetailService;
+    public static OrderDetailService getInstance() {
+        if (orderDetailService == null) {
+            orderDetailService = new OrderDetailService();
+        }
+        return orderDetailService;
+    }
+    public void insertOrderDetail(String ord_id, String prod_id, String prod_name, String prod_color,
+                                  String prod_size, int quantity, double price) {
+        new OrderDetailDAO().insertOrderDetail(ord_id, prod_id, prod_name, prod_color, prod_size, quantity, price);
+    }
+    public List<OrderDetail> getListDetailsFromOrdId(String id) {
+        return new OrderDetailDAO().getListDetailsFromOrdId(id);
+    }
+
+    public void removeOrder(String ord_id) {
+        new OrderDAO().removeOrder(ord_id);
+    }
+
+    public void hardRemoveOrder(String id) {
+        new OrderDAO().hardRemoveOrder(id);
+    }
+
+    public void rollbackOrder(String id) {
+        new OrderDAO().rollbackOrder(id);
+    }
+}
+
